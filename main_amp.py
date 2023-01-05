08df580cac3ace2bea913d2ed989bc0a5d28efac
zjykzj <wy163zhuj@163.com>
2023-01-05 16:02:29 +0800

    perf(main_amp.py): update batch_size/learning_rate/weight_decay

--- main_amp.py
+++ main_amp.py
@@ -60,15 +60,15 @@ def parse():
                         help='number of total epochs to run')
     parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                         help='manual epoch number (useful on restarts)')
-    parser.add_argument('-b', '--batch-size', default=256, type=int,
-                        metavar='N', help='mini-batch size per process (default: 256)')
-    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+    parser.add_argument('-b', '--batch-size', default=64, type=int,
+                        metavar='N', help='mini-batch size per process (default: 64)')
+    parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
                         metavar='LR',
-                        help='Initial learning rate.  Will be scaled by <global batch size>/256: args.lr = args.lr*float(args.batch_size*args.world_size)/256.  A warmup schedule will also be applied over the first 5 epochs.')
+                        help='Initial learning rate.  Will be scaled by <global batch size>/256: args.lr = args.lr*float(args.batch_size*args.world_size)/64.  A warmup schedule will also be applied over the first 5 epochs.')
     parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                         help='momentum')
-    parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
-                        metavar='W', help='weight decay (default: 1e-4)')
+    parser.add_argument('--weight-decay', '--wd', default=1e-5, type=float,
+                        metavar='W', help='weight decay (default: 1e-5)')
     parser.add_argument('--print-freq', '-p', default=10, type=int,
                         metavar='N', help='print frequency (default: 10)')
     parser.add_argument('--resume', default='', type=str, metavar='PATH',
@@ -158,7 +158,7 @@ def main():
     model = model.cuda().to(memory_format=memory_format)
 
     # Scale learning rate based on global batch size
-    args.lr = args.lr * float(args.batch_size * args.world_size) / 256.
+    args.lr = args.lr * float(args.batch_size * args.world_size) / 64.
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
@@ -207,16 +207,16 @@ def main():
         resume()
 
     # Data loading code
-    traindir = os.path.join(args.data, 'train')
-    valdir = os.path.join(args.data, 'val')
-
-    if (args.arch == "inception_v3"):
-        raise RuntimeError("Currently, inception_v3 is not supported by this example.")
-        # crop_size = 299
-        # val_size = 320 # I chose this value arbitrarily, we can adjust.
-    else:
-        crop_size = 224
-        val_size = 256
+    # traindir = os.path.join(args.data, 'train')
+    # valdir = os.path.join(args.data, 'val')
+    #
+    # if (args.arch == "inception_v3"):
+    #     raise RuntimeError("Currently, inception_v3 is not supported by this example.")
+    #     # crop_size = 299
+    #     # val_size = 320 # I chose this value arbitrarily, we can adjust.
+    # else:
+    #     crop_size = 224
+    #     val_size = 256
 
     from cocodataset import COCODataset
     train_dataset = COCODataset('COCO', name='train2017', img_size=608)
@@ -556,9 +556,11 @@ class AverageMeter(object):
 
 def adjust_learning_rate(optimizer, epoch, step, len_epoch):
     """LR schedule that should yield 76% converged accuracy with batch size 256"""
-    factor = epoch // 30
+    # factor = epoch // 30
+    factor = epoch // 10
 
-    if epoch >= 80:
+    # if epoch >= 80:
+    if epoch >= 27:
         factor = factor + 1
 
     lr = args.lr * (0.1 ** factor)
