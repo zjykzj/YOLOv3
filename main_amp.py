578bb4ccbb208bfed149729c535c137867a85146
zjykzj <wy163zhuj@163.com>
2023-01-09 10:41:54 +0800

    style(main_amp.py): remove unused code

--- main_amp.py
+++ main_amp.py
@@ -5,15 +5,12 @@ import time
 import random
 
 import torch
-import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.distributed as dist
 import torch.optim
 import torch.utils.data
 import torch.utils.data.distributed
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
 import torchvision.models as models
 
 import numpy as np
