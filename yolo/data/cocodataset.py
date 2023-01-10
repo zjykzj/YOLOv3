12a3010e124a7d84e0a9c173fb7978f575a4f1f7
zjykzj <wy163zhuj@163.com>
2023-01-10 10:29:19 +0800

    perf(cocodataset): add get_coco_label_names

--- yolo/data/cocodataset.py
+++ yolo/data/cocodataset.py
@@ -19,6 +19,40 @@ from torch.utils.data import Dataset
 from yolo.util.utils import label2yolobox
 
 
+def get_coco_label_names():
+    """
+    COCO label names and correspondence between the model's class index and COCO class index.
+    Returns:
+        coco_label_names (tuple of str) : all the COCO label names including background class.
+        coco_class_ids (list of int) : index of 80 classes that are used in 'instance' annotations
+        coco_cls_colors (np.ndarray) : randomly generated color vectors used for box visualization
+
+    """
+    coco_label_names = ('background',  # class zero
+                        'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck',
+                        'boat', 'traffic light', 'fire hydrant', 'street sign', 'stop sign',
+                        'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+                        'elephant', 'bear', 'zebra', 'giraffe', 'hat', 'backpack', 'umbrella',
+                        'shoe', 'eye glasses', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+                        'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+                        'skateboard', 'surfboard', 'tennis racket', 'bottle', 'plate', 'wine glass',
+                        'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+                        'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
+                        'couch', 'potted plant', 'bed', 'mirror', 'dining table', 'window', 'desk',
+                        'toilet', 'door', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+                        'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'blender', 'book',
+                        'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+                        )
+    coco_class_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20,
+                      21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
+                      46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 67,
+                      70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
+
+    coco_cls_colors = np.random.randint(128, 255, size=(80, 3))
+
+    return coco_label_names, coco_class_ids, coco_cls_colors
+
+
 class COCODataset(Dataset):
 
     def __init__(self, root, name: str = 'train2017', img_size: int = 416, min_size: int = 1,
