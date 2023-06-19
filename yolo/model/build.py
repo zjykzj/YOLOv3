ffe9957ccbcc1c85ba02a72dc688db8b6718dda2
zjykzj <wy163zhuj@163.com>
2023-06-19 14:12:15 +0800

    perf(model): implemented multiple YOLOv3Loss

--- yolo/model/build.py
+++ yolo/model/build.py
@@ -14,7 +14,9 @@ import torch
 
 from .yolov3 import YOLOv3
 # from .yololoss import YOLOv3Loss
-from .yololoss_v2 import YOLOLoss
+# from .yololoss_v2 import YOLOLoss
+# from .yololoss_v3 import YOLOv3Loss
+from .yololoss_v5 import YOLOv3Loss
 
 
 def build_model(args: Namespace, cfg: Dict, device=None):
@@ -50,11 +52,11 @@ def build_criterion(cfg: Dict, device=None):
         #                        obj_scale=cfg['CRITERION']['OBJ_SCALE'],
         #                        class_scale=cfg['CRITERION']['CLASS_SCALE'],
         #                        ).to(device)
-        criterion = YOLOLoss(anchors,
-                             n_classes=cfg['MODEL']['N_CLASSES'],
-                             ignore_thresh=cfg['CRITERION']['IGNORE_THRESH'],
-                             device=device
-                             ).to(device)
+        criterion = YOLOv3Loss(anchors,
+                               n_classes=cfg['MODEL']['N_CLASSES'],
+                               ignore_thresh=cfg['CRITERION']['IGNORE_THRESH'],
+                               device=device
+                               ).to(device)
     else:
         raise ValueError(f"{loss_type} doesn't supports")
     return criterion
--- yolo/model/yololayer_v2.py
+++ yolo/model/yololayer_v2.py
@@ -40,30 +40,22 @@ class YOLOLayer(nn.Module):
         device = outputs.device
 
         # grid coordinate
-        # [W] -> [1, 1, W, 1] -> [B, H, W, num_anchors]
         x_shift = torch.broadcast_to(torch.arange(W).reshape(1, 1, 1, W),
                                      (B, self.num_anchors, H, W)).to(dtype=dtype, device=device)
-        # [H] -> [1, H, 1, 1] -> [B, H, W, num_anchors]
         y_shift = torch.broadcast_to(torch.arange(H).reshape(1, 1, H, 1),
                                      (B, self.num_anchors, H, W)).to(dtype=dtype, device=device)
 
         # broadcast anchors to all grids
-        # [num_anchors] -> [1, 1, 1, num_anchors] -> [B, H, W, num_anchors]
         w_anchors = torch.broadcast_to(self.masked_anchors[:, 0].reshape(1, self.num_anchors, 1, 1),
                                        (B, self.num_anchors, H, W)).to(dtype=dtype, device=device)
         h_anchors = torch.broadcast_to(self.masked_anchors[:, 1].reshape(1, self.num_anchors, 1, 1),
                                        (B, self.num_anchors, H, W)).to(dtype=dtype, device=device)
 
         # Reshape
-        # [B, num_anchors * (4+1+num_classes), H, W] ->
-        # [B, H, W, num_anchors * (4+1+num_classes)] ->
-        # [B, H, W, num_anchors, 4+1+num_classes]
-        # outputs = outputs.permute(0, 2, 3, 1).reshape(B, H, W, self.num_anchors, n_ch)
         outputs = outputs.reshape(B, self.num_anchors, n_ch, H, W).permute(0, 1, 3, 4, 2)
 
         # logistic activation
         # xy + obj_pred + cls_pred
-        # outputs[..., np.r_[:2, 4:5]] = torch.sigmoid(outputs[..., np.r_[:2, 4:5]])
         outputs[..., np.r_[:2, 4:n_ch]] = torch.sigmoid(outputs[..., np.r_[:2, 4:n_ch]])
 
         # b_x = sigmoid(t_x) + c_x
@@ -71,16 +63,10 @@ class YOLOLayer(nn.Module):
         # b_w = exp(t_w) * p_w
         # b_h = exp(t_h) * p_h
         #
-        # 预测框坐标x0加上每个网格的左上角坐标x
         outputs[..., 0] += x_shift
-        # 预测框坐标y0加上每个网格的左上角坐标y
         outputs[..., 1] += y_shift
-        # 计算预测框长/宽的实际长度
         outputs[..., 2] = torch.exp(outputs[..., 2]) * w_anchors
         outputs[..., 3] = torch.exp(outputs[..., 3]) * h_anchors
-
-        # 分类概率压缩
-        # outputs[..., 5:] = torch.softmax(outputs[..., 5:], dim=-1)
 
         # 推理阶段，不计算损失. 将预测框坐标按比例返回到原图大小
         outputs[..., :4] *= self.stride
